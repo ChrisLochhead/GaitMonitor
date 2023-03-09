be470ac1f9d5b6b4f79ae28259b09166ca6dca5b
Chris Lochhead <chrislochhead123@outlook.com>
2023-03-09 12:40:22 +0000

    Depth calculations conclusively fixed, debug functions also implemented for displaying joints

--- Code/Programs/simple-HigherHRNet/main.py
+++ Code/Programs/simple-HigherHRNet/main.py
@@ -8,6 +8,7 @@ import csv
 import copy
 import pandas as pd
 from array import array
+import ast
 
 from ast import literal_eval
 import pyrealsense2 as rs
@@ -34,15 +35,31 @@ def save(joints):
 
 def load(file = "image_data.csv"):
     joints = []
-    person = []
-    with open("image_data.csv") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            person.append(row)
-        joints.append(person)
-    
+    #Load in as a pandas dataset
+    colnames=['Instance', 'No_In_Sequence', 'Class', 'Joint_1','Joint_2','Joint_3','Joint_4','Joint_5','Joint_6','Joint_7',
+          'Joint_8','Joint_9','Joint_10','Joint_11','Joint_12','Joint_13','Joint_14','Joint_15','Joint_16', 'Joint_17'] 
+    dataset = pd.read_csv(file, names=colnames, header=None)
+
+    #Convert all data to literals
+    dataset = convert_to_literals(dataset)
+
+    #Convert to 2D array 
+    joints = dataset.to_numpy()
+    #Print array to check
+    print("these are the joints\n", joints)
     return joints
+
+def convert_to_literals(data):
+    for i,  (index, row) in enumerate(data.iterrows()):
+        for col_index, col in enumerate(row):
+            if col_index >= 3:
+                print("converting: ", row[col_index])
+                tmp = ast.literal_eval(row[col_index])
+                data.iat[i, col_index] = copy.deepcopy(tmp)
+            else:
+                data.iat[i, col_index] = int(data.iat[i, col_index])
+
+    return data
 
 def run_video():
     print("initialising model")
@@ -115,36 +132,37 @@ def make_intrinsics(intrinsics_file = "depth_intrinsics.csv"):
 
 loaded_intrinsics = make_intrinsics()
 
-def get_3D_coords(coords_2d, dep_img, pts3d_net = True, dilate = True, excl_2D = False ):
+def get_3D_coords(coords_2d, dep_img, pts3d_net = True, dilate = True, meta_data = 3):
 
     pts_3D = []
+    pts_3D_metres = []
     orig_dep_img = copy.deepcopy(dep_img)
     kernel = np.ones((6,6), np.uint8)
     if dilate == True:
         dep_img = cv2.dilate(dep_img, kernel, cv2.BORDER_REFLECT)
 
-    dep_img = np.transpose(dep_img)
-    for i in range(3, len(coords_2d)):
+    #orig_dep_img = np.transpose(orig_dep_img)
+    for i in range(meta_data, len(coords_2d)):
         
-        x = int(coords_2d[i][0]); y = int(coords_2d[i][1])
-
-        #Deal with any cut-off joints from the frame
-        if y >= 424:
-            y = 423
-        if x >= 240:
-            x = 239
-
+        x = copy.deepcopy(int(coords_2d[i][0])); y = copy.deepcopy(int(coords_2d[i][1]))
+        
         if pts3d_net == True:
-            result = rs.rs2_deproject_pixel_to_point(loaded_intrinsics, [x, y], dep_img[y, x])
-            if excl_2D == False:
-                pts_3D.append([-result[0], -result[1], result[2]])
-            else:
-                pts_3D.append([x, y, result[2]])
+            
+            #Clamp joints within frame size
+            if x >= 240:
+                x= 239
+            if y >= 424:
+                y= 423
+
+            result = rs.rs2_deproject_pixel_to_point(loaded_intrinsics, [x, y], orig_dep_img[(x, y)])
+            pts_3D_metres.append([-result[0], -result[1], result[2]])
+            pts_3D.append([x, y, result[2]])
 
         else:
             pts_3D.append([x,y, dep_img[(y,x)]])
+            pts_3D_metres.append([x,y, dep_img[(y,x)]])
 
-    return pts_3D
+    return pts_3D, pts_3D_metres
 
 def get_joints_from_frame(model, frame, anonymous = True):
     joints = model.predict(frame)
@@ -170,16 +188,39 @@ def get_joints_from_frame(model, frame, anonymous = True):
 
     return frame, joints
 
+def test_loader(directory = "./Images"):
+    joints = load("gait_dataset_pixels.csv")
+    instance_limit = 2
+    subdir_iter = 1
+    joint_iter = 0
+    for i, (subdir, dirs, files) in enumerate(os.walk(directory)):
+        
+        for j, file in enumerate(files):
+            #Ignore depth images which are second half
+            if j >= len(files)/2:
+                break
+
+            file_name = os.fsdecode(file)
+            sub_dir = os.fsdecode(subdir)
+        
+            #display with openCV original image, overlayed with corresponding joints
+            raw_image = cv2.imread(sub_dir + "/" + file_name, cv2.IMREAD_COLOR)
+            #i - 2 because iter starts at 1, and the first empty subdir also counts as 1.
+            print("these are the joints: ", joints[joint_iter])
+            render_joints(raw_image, joints[joint_iter], delay = True, use_depth = True)
+            joint_iter += 1
+        subdir_iter += 1
+        if subdir_iter >= 4:
+            break
+
 def run_image(image_name, single = True, save = False, directory = None, model= None, image_no = 0):
     if model == None:
         model = SimpleHigherHRNet(32, 17, "./weights/pose_higher_hrnet_w32_512.pth")
     image = cv2.imread(image_name, cv2.IMREAD_COLOR)
 
-
-    #Test loading function
-    #joints = load("image_data.csv")
     #apply joints to image for visualisation
     image, joints = get_joints_from_frame(model, image, anonymous=True)
+
 
     loop = True
     while loop == True:
@@ -197,8 +238,21 @@ def run_image(image_name, single = True, save = False, directory = None, model= 
 def draw_joints_on_frame(frame, joints, use_depth_as_colour = False):
 
     tmp_frame = copy.deepcopy(frame)
-    for joint in joints:
+    tmp_joints = copy.deepcopy(joints)
+
+    for joint in tmp_joints:
+
+        if isinstance(joint, int):
+            continue
         #0 is X, Y is 1, 2 is confidence.
+
+        #Clamp joints within frame size
+        
+        if joint[0] >= 240:
+            joint[0] = 239
+        if joint[1] >= 424:
+            joint[1] = 423
+            
         if use_depth_as_colour == False:
             tmp_frame = cv2.circle(tmp_frame, (int(float(joint[1])),int(float(joint[0]))), radius=1, color=(0, 0, 255), thickness=4)
         else:
@@ -249,7 +303,7 @@ def run_depth_sample(folder_name, joints_info):
 
             #Apply depth changes to data, firstly with 2d images using z as colour
             #Need corresponding depth image not raw
-            refined_joint_set = get_3D_coords(joint_set, depth_image, excl_2D=True)
+            refined_joint_set, rjs_metres = get_3D_coords(joint_set, depth_image)
             refined_joint_image = draw_joints_on_frame(raw_image, refined_joint_set, use_depth_as_colour=True)
             cv2.imshow('image with refined joints (excl 2D)',refined_joint_image)
             cv2.setMouseCallback('image with refined joints (excl 2D)', click_event, refined_joint_image)
@@ -257,12 +311,21 @@ def run_depth_sample(folder_name, joints_info):
 
 
             #display again with 3d positions including altered X and Y
-            final_joint_set = get_3D_coords(joint_set, depth_image, excl_2D=False)
-            final_joint_image = draw_joints_on_frame(raw_image, final_joint_set, use_depth_as_colour=True)
+            final_joint_set, fjs_metres = get_3D_coords(joint_set, depth_image)
+            final_joint_image = draw_joints_on_frame(raw_image, fjs_metres, use_depth_as_colour=True)
             cv2.imshow('image with refined joints (incl 2D)',final_joint_image)
             cv2.setMouseCallback('image with refined joints (incl 2D)', click_event, final_joint_image)
             cv2.waitKey(0) & 0xff
 
+
+def render_joints(image, joints, delay = False, use_depth = True):
+    tmp_image = copy.deepcopy(image)
+    tmp_image = draw_joints_on_frame(tmp_image, joints, use_depth_as_colour=use_depth)
+    cv2.imshow('joint Image',tmp_image)
+
+    cv2.setMouseCallback('joint Image', click_event, tmp_image)
+    if delay:
+        cv2.waitKey(0) & 0xff
 
 def click_event(event, x, y, flags, params):
   
@@ -296,9 +359,11 @@ def run_images(folder_name, exclude_2D = False):
         first_depth = True
         count_in_directory = 0
 
-        for file in files:
+        for f, file in enumerate(files):
 
             file_name = os.fsdecode(file)
+            if file_name[0] == 'd':
+                continue
             sub_dir = os.fsdecode(subdir)
             print("Sub directory: ", sub_dir, " Instance: ", file_iter - count_in_directory)
 
@@ -306,73 +371,60 @@ def run_images(folder_name, exclude_2D = False):
             
             os.makedirs(out_directory, exist_ok=True)
 
-            #Once depth file has been found
-            if file_name[0] == 'd':
-                if first_depth == True:
-                    count_in_directory = file_iter
-                    first_depth = False
-                    
-                #Load depth image
-                dep_image = cv2.imread(sub_dir + "/" + file_name, cv2.IMREAD_ANYDEPTH)
+             #Get joints from initial image
+            image, joints = run_image(sub_dir + "/" + file_name, single=False, save = True, directory=out_directory + sub_dir, model=model, image_no = file_iter)
 
-                #Find corresponding raw image and extract co-ordinates
-                joint_iter = 0
+            # Get corresponding depth image, this is always same index += half the length of the instance
+            dep_image = cv2.imread(sub_dir + "/" + os.fsdecode(files[int(f + (len(files)/2))]), cv2.IMREAD_ANYDEPTH)
+            corr_image = cv2.imread(sub_dir + "/" + file_name, cv2.IMREAD_ANYCOLOR )
 
-                refined_joints = get_3D_coords(joints_file[file_iter - count_in_directory], dep_image, excl_2D=False)
-                refined_joints_metres = get_3D_coords(joints_file_metres[file_iter - count_in_directory], dep_image, excl_2D=True)
-
-                #print("what is joints file: ", joints_file[file_iter - count_in_directory])
-                for i, dep_joint in enumerate(refined_joints):
-                    if i >= 3:
-
-                        #Make sure this isnt backwards during EDA
-                        if int(dep_joint[0]) >= 424:
-                            dep_joint[0] = 423
-                        if int(dep_joint[1]) >= 240:
-                            dep_joint[1] = 239
-
-                        zDepth = dep_image[int(dep_joint[1])][int(dep_joint[0])]
-    
-                        #Fix this to record into the right place and then you are done
-                        dep_joint = [dep_joint[0], dep_joint[1], zDepth]#[int(dep_joint[0]), int(dep_joint[1]), int(zDepth)]
-                        joints_file[file_iter - count_in_directory][i] = dep_joint
-                        joints_file_metres[file_iter - count_in_directory][i] = refined_joints_metres[i]
-
-
+            if len(joints) > 0:
+                if len(joints[0]) < 1:
+                    joints = [ [0,0,0] for _ in range(17) ]
+            
+            if len(joints) > 0:
+                refined_joints, refined_joints_metres = get_3D_coords(joints[0], dep_image, meta_data=0)
+                print("found refined joints no: ", len(refined_joints))
             else:
-                image, joints = run_image(sub_dir + "/" + file_name, single=False, save = True, directory=out_directory + sub_dir, model=model, image_no = file_iter)
-                #print("joints returned: ", joints)
-                if len(joints) < 1:
-                    joints = [[ [0,0,0] for _ in range(17) ]]
-                
-                new_entry = [subdir_iter, file_iter, data_class]
-                # 0 is instance, 1 is num in sequence, 2 is class, 3 is array of joints
-                for i, joint in enumerate(joints[0]):
-                    #Convert so it saves with comma delimiters within the joint-sets
-                    tmp = [joint[0], joint[1], joint[2]]
-                    new_entry.append(tmp)
+                refined_joints = [ [0,0,0] for _ in range(17) ]
+                refined_joints_metres = [ [0,0,0] for _ in range(17) ]
 
-                joints_file.append(new_entry)
-                joints_file_metres.append(new_entry)
+            new_entry = [subdir_iter, file_iter, data_class]
+
+            # 0 is instance, 1 is num in sequence, 2 is class, 3 is array of joints
+            for i, joint in enumerate(refined_joints):
+                #Convert so it saves with comma delimiters within the joint-sets
+                tmp = [joint[0], joint[1], joint[2]]
+                new_entry.append(tmp)
+
+            print("full completed depth joints: ", new_entry)
+            render_joints(corr_image, new_entry[3:], delay=True, use_depth=True)
+            
+            joints_file.append(new_entry)
+            joints_file_metres.append(new_entry)
+
             file_iter += 1
         subdir_iter +=1
+        if subdir_iter >= 4:
+            print("BREAKING")
+            break
         file_iter = 0
     #Save to .csv
     print("SAVING")
-    if exclude_2D == True:
-        with open("gait_dataset_pixels.csv","w+", newline='') as my_csv:
-            csvWriter = csv.writer(my_csv,delimiter=',')
-            csvWriter.writerows(joints_file)
-    else:
-        with open("gait_dataset_metres.csv","w+", newline='') as my_csv:
-            csvWriter = csv.writer(my_csv,delimiter=',')
-            csvWriter.writerows(joints_file_metres)
+
+    with open("gait_dataset_pixels.csv","w+", newline='') as my_csv:
+        csvWriter = csv.writer(my_csv,delimiter=',')
+        csvWriter.writerows(joints_file)
+
+    with open("gait_dataset_metres.csv","w+", newline='') as my_csv:
+        csvWriter = csv.writer(my_csv,delimiter=',')
+        csvWriter.writerows(joints_file_metres)
 
 
 def main():
-    run_images("./Images", exclude_2D=True)
-    print("Running images excluding metres x and y completed, running metres now.")
-    run_images("./Images", exclude_2D=False)
+    #run_images("./Images")
+    test_loader()
+    #run_images("./Images", exclude_2D=False)
     #run_depth_sample("./DepthExamples", "depth_examples.csv")
     #run_video()
 if __name__ == '__main__':
