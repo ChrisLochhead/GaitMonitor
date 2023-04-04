25404b75c32a6907d87b65a7c60ec4a35bc4b7d3
Chris Lochhead <chrislochhead123@outlook.com>
2023-04-04 10:08:56 +0100

    removed hanging init files

--- Code/Programs/Data_Analysis/GCN/GCN.py
+++ Code/Programs/Data_Analysis/GCN/GCN.py
@@ -5,15 +5,15 @@ import torch
 # Visualization libraries
 import matplotlib.pyplot as plt
 import networkx as nx
-from torch_geometric.datasets import KarateClub
+from torch_geometric.datasets import KarateClub, PascalVOCKeypoints
 from torch_geometric.utils import to_dense_adj
 from torch_geometric.utils import to_networkx
 from torch.nn import Linear
 from torch_geometric.nn import GCNConv
+import pandas as pd
 #%%capture
 from IPython.display import display, HTML
 from matplotlib import animation
-
 plt.rcParams["animation.bitrate"] = 3000
 plt.rcParams['animation.ffmpeg_path'] = "C:/Users/Chris/Desktop/ffmpeg-5.1.2-full_build/bin/ffmpeg.exe"
 
@@ -134,10 +134,12 @@ def animate_alt(i, *fargs):
 
 def main():
     # Import dataset from PyTorch Geometric
-    dataset = KarateClub()
+    #dataset = KarateClub()
+    dataset = PascalVOCKeypoints("./Datasets", "Person")
 
     # Print information for whole graph from data
     print(dataset)
+    print("type: ", type(dataset), type(dataset[0]))
     print('------------')
     print(f'Number of graphs: {len(dataset)}')
     print(f'Number of features: {dataset.num_features}')
@@ -217,4 +219,146 @@ def main():
     plt.show()
 
 if __name__ == "__main__":
+    #Step 1: Download data:
+    #colnames=['Instance', 'No_In_Sequence', 'Class', 'Joint_1','Joint_2','Joint_3','Joint_4','Joint_5','Joint_6','Joint_7',
+    #      'Joint_8','Joint_9','Joint_10','Joint_11','Joint_12','Joint_13','Joint_14','Joint_15','Joint_16', 'Joint_17'] #
+
+#    dataset_master = pd.read_csv("pixel_data_absolute.csv", names =colnames, header=None )
+#    print(dataset_master.head())
     main()
+
+
+#%%
+
+import pandas as pd
+import torch
+import torch_geometric
+from torch_geometric.data import Dataset
+import numpy as np 
+import os
+from tqdm import tqdm
+#import deepchem as dc
+#from rdkit import Chem 
+
+print(f"Torch version: {torch.__version__}")
+print(f"Cuda available: {torch.cuda.is_available()}")
+print(f"Torch geometric version: {torch_geometric.__version__}")
+
+class JointDataset(Dataset):
+    def __init__(self, root, filename, test=False, transform=None, pre_transform=None):
+        """
+        root = Where the dataset should be stored. This folder is split
+        into raw_dir (downloaded dataset) and processed_dir (processed data). 
+        """
+        self.test = test
+        self.filename = filename
+        super(JointDataset, self).__init__(root, transform, pre_transform)
+        
+    @property
+    def raw_file_names(self):
+        """ If this file exists in raw_dir, the download is not triggered.
+            (The download func. is not implemented here)  
+        """
+        return self.filename
+
+    @property
+    def processed_file_names(self):
+        """ If these files are found in raw_dir, processing is skipped"""
+        self.data = pd.read_csv(self.raw_paths[0]).reset_index()
+
+        if self.test:
+            return [f'data_test_{i}.pt' for i in list(self.data.index)]
+        else:
+            return [f'data_{i}.pt' for i in list(self.data.index)]
+        
+
+    def download(self):
+        pass
+
+    def process(self):
+        self.data = pd.read_csv(self.raw_paths[0]).reset_index()
+        featurizer = dc.feat.MolGraphConvFeaturizer(use_edges=True)
+        for index, row in tqdm(self.data.iterrows(), total=self.data.shape[0]):
+            # Featurize molecule
+            #mol = Chem.MolFromSmiles(row["smiles"])
+            #f = featurizer._featurize(mol)
+            #data = f.to_pyg_graph()
+            data = Data
+
+            #Change data.x to one video per instance for 60 instances, each having 1 class label and 1 instance as metadata 
+
+            data.instance = row[0]
+            data.no_in_sequence = row[2]
+            data.x = row.iloc[3:]
+            data.y = self._get_label(row[2])
+
+
+            if self.test:
+                torch.save(data, 
+                    os.path.join(self.processed_dir, 
+                                 f'data_test_{index}.pt'))
+            else:
+                torch.save(data, 
+                    os.path.join(self.processed_dir, 
+                                 f'data_{index}.pt'))
+            
+
+    def _get_label(self, label):
+        label = np.asarray([label])
+        return torch.tensor(label, dtype=torch.int64)
+
+    def len(self):
+        return self.data.shape[0]
+
+    def get(self, idx):
+        """ - Equivalent to __getitem__ in pytorch
+            - Is not needed for PyG's InMemoryDataset
+        """
+        if self.test:
+            data = torch.load(os.path.join(self.processed_dir, 
+                                 f'data_test_{idx}.pt'))
+        else:
+            data = torch.load(os.path.join(self.processed_dir, 
+                                 f'data_{idx}.pt'))        
+        return data
+
+#%%
+
+class MyOwnDataset(Dataset):
+    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+        super().__init__(root, transform, pre_transform, pre_filter)
+
+    @property
+    def raw_file_names(self):
+        return ['some_file_1', 'some_file_2', ...]
+
+    @property
+    def processed_file_names(self):
+        return ['data_1.pt', 'data_2.pt', ...]
+
+    def download(self):
+        # Download to `self.raw_dir`.
+        path = download_url(url, self.raw_dir)
+        ...
+
+    def process(self):
+        idx = 0
+        for raw_path in self.raw_paths:
+            # Read data from `raw_path`.
+            data = Data(...)
+
+            if self.pre_filter is not None and not self.pre_filter(data):
+                continue
+
+            if self.pre_transform is not None:
+                data = self.pre_transform(data)
+
+            torch.save(data, osp.join(self.processed_dir, f'data_{idx}.pt'))
+            idx += 1
+
+    def len(self):
+        return len(self.processed_file_names)
+
+    def get(self, idx):
+        data = torch.load(osp.join(self.processed_dir, f'data_{idx}.pt'))
+        return data
