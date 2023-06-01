1514ed78aadbbb63761977bf2f6907d31009b48b
Chris Lochhead <chrislochhead123@outlook.com>
2023-06-01 18:16:24 +0100

    working on autoencoder

--- Code/Programs/Machine_Learning/Model_Based/AutoEncoder/GAE.py
+++ Code/Programs/Machine_Learning/Model_Based/AutoEncoder/GAE.py
@@ -1,86 +1,287 @@
-import argparse
-import os.path as osp
-
+import matplotlib.pyplot as plt # plotting library
+import numpy as np # this module is useful to work with numerical arrays
+import pandas as pd 
+import random 
 import torch
+import torchvision
+from torchvision import transforms
+from torch.utils.data import DataLoader,random_split
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+import tqdm
+from torch_geometric.nn import GCNConv, global_add_pool, GINConv, GATv2Conv
 
-import torch_geometric.transforms as T
-from torch_geometric.datasets import Planetoid
-from torch_geometric.nn import GAE, VGAE, GCNConv
+from sklearn.manifold import TSNE
+import plotly.express as px
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+class VariationalEncoder(nn.Module):
+    def __init__(self, dim_in, dim_h, latent_dims, heads=8):  
+        super(VariationalEncoder, self).__init__()
+        self.conv1 = GATv2Conv(dim_in, dim_h, heads=heads)
+        self.conv2 = GATv2Conv(dim_in, dim_h, heads=heads)
+        #self.conv1 = nn.Conv2d(1, 8, 3, stride=2, padding=1)
+        #self.conv2 = nn.Conv2d(8, 16, 3, stride=2, padding=1)
+        self.batch2 = nn.BatchNorm2d(16)
+
+        self.conv3 = GATv2Conv(dim_in, dim_h, heads=heads)
+        #self.conv3 = nn.Conv2d(16, 32, 3, stride=2, padding=0) 
+        #  
+        self.linear1 = nn.Linear(3*3*32, 128)
+        self.linear2 = nn.Linear(128, latent_dims)
+        self.linear3 = nn.Linear(128, latent_dims)
+
+        self.N = torch.distributions.Normal(0, 1)
+        self.N.loc = self.N.loc.cuda() # hack to get sampling on the GPU
+        self.N.scale = self.N.scale.cuda()
+        self.kl = 0
+
+    def forward(self, x, edge_index):
+        #THIS IS THE STANDARD GCN FORWARD FUNCTION
+        # Node embeddings
+        x = x.to("cuda")
+        edge_index = edge_index.to("cuda")
+        batch = batch.to("cuda")
+
+        h1 = self.gcn1(x, edge_index)
+        h2 = self.gcn2(h1, edge_index)
+
+        # Graph-level readout
+        h1 = global_add_pool(h1, batch)
+        h2 = global_add_pool(h2, batch)
+        # Concatenate graph embeddings
+        h = torch.cat((h1, h2), dim=1)
+
+        x = torch.flatten(h, start_dim=1)
+        x = F.relu(self.linear1(x))
+        mu =  self.linear2(x)
+        sigma = torch.exp(self.linear3(x))
+        z = mu + sigma*self.N.sample(mu.shape)
+        self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+        return z      
+        # Classifier
+        #print("H dimensions: ", h.shape)
+        h = self.lin1(h)
+        h = h.relu()
+        h = F.dropout(h, p=0.5, training=self.training)
+        h = self.lin2(h)
+
+        return h, F.log_softmax(h, dim=1)
+        #This is the original GAE FORWARD
+        '''
+        x = x.to(device)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.batch2(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.linear1(x))
+        mu =  self.linear2(x)
+        sigma = torch.exp(self.linear3(x))
+        z = mu + sigma*self.N.sample(mu.shape)
+        self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+        return z      
 '''
-parser = argparse.ArgumentParser()
-parser.add_argument('--variational', action='store_true')
-parser.add_argument('--linear', action='store_true')
-parser.add_argument('--dataset', type=str, default='Cora',
-                    choices=['Cora', 'CiteSeer', 'PubMed'])
-parser.add_argument('--epochs', type=int, default=400)
-args = parser.parse_args()
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-transform = T.Compose([
-    T.NormalizeFeatures(),
-    T.ToDevice(device),
-    T.RandomLinkSplit(num_val=0.05, num_test=0.1, is_undirected=True,
-                      split_labels=True, add_negative_train_samples=False),
-])
-path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Planetoid')
-dataset = Planetoid(path, args.dataset, transform=transform)
-train_data, val_data, test_data = dataset[0]
-'''
 
-class GCNEncoder(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
+
+class Decoder(nn.Module):
+    
+    def __init__(self, latent_dims):
         super().__init__()
-        self.conv1 = GCNConv(in_channels, 2 * out_channels)
-        self.conv2 = GCNConv(2 * out_channels, out_channels)
+
+        self.decoder_lin = nn.Sequential(
+            nn.Linear(latent_dims, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 3 * 3 * 32),
+            nn.ReLU(True)
+        )
+
+        self.unflatten = nn.Unflatten(dim=1, unflattened_size=(32, 3, 3))
+
+        self.decoder_conv = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, 3, stride=2, output_padding=0),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 1, 3, stride=2, padding=1, output_padding=1)
+        )
+        
+    def forward(self, x):
+        x = self.decoder_lin(x)
+        x = self.unflatten(x)
+        x = self.decoder_conv(x)
+        x = torch.sigmoid(x)
+        return x
+    
+
+
+class VariationalAutoencoder(nn.Module):
+    def __init__(self, dim_in, dim_h, latent_dims):
+        super(VariationalAutoencoder, self).__init__()
+        self.encoder = VariationalEncoder(dim_in, dim_h, latent_dims)
+        self.decoder = Decoder(latent_dims)
 
     def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index).relu()
-        return self.conv2(x, edge_index)
+        x = x.to(device)
+        z = self.encoder(x, edge_index)
+        return self.decoder(z)
+    
 
 
-class VariationalGCNEncoder(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.conv1 = GCNConv(in_channels, 2 * out_channels)
-        self.conv_mu = GCNConv(2 * out_channels, out_channels)
-        self.conv_logstd = GCNConv(2 * out_channels, out_channels)
+### Training function
+def train_epoch(vae, device, dataloader, optimizer):
+    # Set train mode for both the encoder and the decoder
+    vae.train()
+    train_loss = 0.0
+    # Iterate the dataloader (we do not need the label values, this is unsupervised learning)
+    for x in dataloader: 
+        # Move tensor to the proper device
+        x = x.to(device)
+        x_hat = vae(x.x, x.edge_index)
+        # Evaluate loss
+        loss = ((x - x_hat)**2).sum() + vae.encoder.kl
 
-    def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index).relu()
-        return self.conv_mu(x, edge_index), self.conv_logstd(x, edge_index)
+        # Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # Print batch loss
+        print('\t partial train loss (single batch): %f' % (loss.item()))
+        train_loss+=loss.item()
 
-
-class LinearEncoder(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.conv = GCNConv(in_channels, out_channels)
-
-    def forward(self, x, edge_index):
-        return self.conv(x, edge_index)
-
-
-class VariationalLinearEncoder(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.conv_mu = GCNConv(in_channels, out_channels)
-        self.conv_logstd = GCNConv(in_channels, out_channels)
-
-    def forward(self, x, edge_index):
-        return self.conv_mu(x, edge_index), self.conv_logstd(x, edge_index)
-
-def GAEtrain(model, optimizer, train_data):
-    model.train()
-    optimizer.zero_grad()
-    z = model.encode(train_data.x, train_data.edge_index)
-    loss = model.recon_loss(z, train_data.pos_edge_label_index)
-    loss = loss + (1 / train_data.num_nodes) * model.kl_loss()
-    loss.backward()
-    optimizer.step()
-    return float(loss)
+    return train_loss / len(dataloader.dataset)
 
 
-@torch.no_grad()
-def GAEtest(data, model):
-    model.eval()
-    z = model.encode(data.x, data.edge_index)
-    return model.test(z, data.pos_edge_label_index, data.neg_edge_label_index)
+
+### Testing function
+def test_epoch(vae, device, dataloader):
+    # Set evaluation mode for encoder and decoder
+    vae.eval()
+    val_loss = 0.0
+    with torch.no_grad(): # No need to track the gradients
+        for x, _ in dataloader:
+            # Move tensor to the proper device
+            x = x.to(device)
+            # Encode data
+            encoded_data = vae.encoder(x)
+            # Decode data
+            x_hat = vae(x)
+            loss = ((x - x_hat)**2).sum() + vae.encoder.kl
+            val_loss += loss.item()
+
+    return val_loss / len(dataloader.dataset)
+
+
+
+def plot_ae_outputs(encoder,decoder,n=10, test_dataset=None):
+    plt.figure(figsize=(16,4.5))
+    targets = test_dataset.targets.numpy()
+    t_idx = {i:np.where(targets==i)[0][0] for i in range(n)}
+    for i in range(n):
+      ax = plt.subplot(2,n,i+1)
+      img = test_dataset[t_idx[i]][0].unsqueeze(0).to(device)
+      encoder.eval()
+      decoder.eval()
+      with torch.no_grad():
+         rec_img  = decoder(encoder(img))
+      plt.imshow(img.cpu().squeeze().numpy(), cmap='gist_gray')
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)  
+      if i == n//2:
+        ax.set_title('Original images')
+      ax = plt.subplot(2, n, i + 1 + n)
+      plt.imshow(rec_img.cpu().squeeze().numpy(), cmap='gist_gray')  
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)  
+      if i == n//2:
+         ax.set_title('Reconstructed images')
+    plt.show()  
+
+
+
+def show_image(img):
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+def run_VAE(train_loader, val_loader, test_dataset):
+
+    data_dir = 'dataset'
+
+    #train_dataset = torchvision.datasets.MNIST(data_dir, train=True, download=True)
+    #test_dataset  = torchvision.datasets.MNIST(data_dir, train=False, download=True)
+
+    transform = transforms.Compose([
+    transforms.ToTensor(),
+    ])
+
+    #dataset.transform = transform
+    #test_dataset.transform = test_transform
+
+    #m=len(dataset)
+
+    #train_data, val_data = random_split(train_dataset, [int(m-m*0.2), int(m*0.2)])
+    #batch_size=10#256
+
+    #train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size)
+    #valid_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size)
+    #test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,shuffle=True)
+
+    ### Set the random seed for reproducible results
+    torch.manual_seed(0)
+    d = 9
+    vae = VariationalAutoencoder(dim_in=3, dim_h=16, latent_dims=d)
+    lr = 1e-3 
+    optim = torch.optim.Adam(vae.parameters(), lr=lr, weight_decay=1e-5)
+    print(f'Selected device: {device}')
+    vae.to(device)
+    vae.eval()
+    num_epochs = 50
+
+    for epoch in range(num_epochs):
+        train_loss = train_epoch(vae,device,train_loader,optim)
+        val_loss = test_epoch(vae,device,val_loader)
+        print('\n EPOCH {}/{} \t train loss {:.3f} \t val loss {:.3f}'.format(epoch + 1, num_epochs,train_loss,val_loss))
+        plot_ae_outputs(vae.encoder,vae.decoder,n=10, test_dataset=test_dataset)
+
+    with torch.no_grad():
+
+        # sample latent vectors from the normal distribution
+        latent = torch.randn(128, d, device=device)
+
+        # reconstruct images from the latent vectors
+        img_recon = vae.decoder(latent)
+        img_recon = img_recon.cpu()
+
+        fig, ax = plt.subplots(figsize=(20, 8.5))
+        show_image(torchvision.utils.make_grid(img_recon.data[:100],10,5))
+        plt.show()
+
+
+    encoded_samples = []
+    for sample in tqdm(test_dataset):
+        img = sample[0].unsqueeze(0).to(device)
+        label = sample[1]
+        # Encode image
+        vae.eval()
+        with torch.no_grad():
+            encoded_img  = vae.encoder(img)
+        # Append to list
+        encoded_img = encoded_img.flatten().cpu().numpy()
+        encoded_sample = {f"Enc. Variable {i}": enc for i, enc in enumerate(encoded_img)}
+        encoded_sample['label'] = label
+        encoded_samples.append(encoded_sample)
+        
+    encoded_samples = pd.DataFrame(encoded_samples)
+    encoded_samples
+
+
+    px.scatter(encoded_samples, x='Enc. Variable 0', y='Enc. Variable 1', color=encoded_samples.label.astype(str), opacity=0.7)
+
+    tsne = TSNE(n_components=2)
+    tsne_results = tsne.fit_transform(encoded_samples.drop(['label'],axis=1))
+
+    fig = px.scatter(tsne_results, x=0, y=1, color=encoded_samples.label.astype(str),labels={'0': 'tsne-2d-one', '1': 'tsne-2d-two'})
+    fig.show()
