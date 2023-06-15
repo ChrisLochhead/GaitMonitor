fixed hcf data generation and normalizing

Code/Programs/Data_Processing/Model_Based/Dataset_Creator.py
@@ -9,8 +9,12 @@ import copy
 
 from sklearn.preprocessing import normalize
 
-def normalize_values(data, joint_output):
-    data, _ = Utilities.process_data_input(data, None, cols=Utilities.colnames)
+def normalize_values(data, joint_output, hcf = False):
+    if hcf:
+        data, _ = Utilities.process_data_input(data, None, cols=Utilities.hcf_colnames)
+    else:
+        data, _ = Utilities.process_data_input(data, None, cols=Utilities.colnames)
+    print("data length: ", len(data[0]))
     meta_data = []
     joint_data = []
 
@@ -18,39 +22,50 @@ def normalize_values(data, joint_output):
         if i < 3: 
             meta_data.append(column)
         else:
-            print("column before: ", len(column), len(column[0]))
-            normed_matrix = normalize(column, axis=1, norm='l2')  
-            print("column after: ", len(normed_matrix), len(normed_matrix[0]))
+            #print("column before: ", len(column), column)
+            if hcf:
+                normed_matrix = Utilities.normalize_1D(column)
+                #normed_matrix = normalize(column, axis=0, norm='l2')  
+            else:
+                normed_matrix = normalize(column, axis=1, norm='l2')  
+            #print("column after: ", len(normed_matrix), len(normed_matrix[0]))
             joint_data.append(normed_matrix)
             #print("lens: ", len(normed_matrix), len(column), len(column[0]))
 
+    print("joint data: ", len(joint_data[0]))
     transposed_joints = []
     for index, j in enumerate(zip(*joint_data)):
         transposed_joints.append(j)
 
     joint_data = transposed_joints
-    print("joint data info: ", len(joint_data), len(joint_data[0]), len(joint_data[0][0]))
+    #print("joint data info: ", len(joint_data), len(joint_data[0]), len(joint_data[0][0]))
     meta_data = np.transpose(meta_data)
-
+    print("meta data: ", len(meta_data[0]))
     final = []
-    print("meta data shape: ", len(meta_data), len(meta_data[1]))
+    #print("meta data shape: ", len(meta_data), len(meta_data[1]))
     for j, col in enumerate(meta_data):
         final_row = list(col)
         for coords in joint_data[j]:
             #Fix the formatting before saving it 
-            refactored_coords = []
-            for c in coords:
-                refactored_coords.append(c)
-            final_row.append(refactored_coords)
+            if hcf == False:
+                refactored_coords = []
+                for c in coords:
+                    refactored_coords.append(c)
+                final_row.append(refactored_coords)
+            else:
+                final_row.append(coords)
 
         final.append(final_row)
    
 
-    print("joints: ", len(final))
-    print("joints dimensions: ", len(final[0]))
-    print(len(final[0][4]))
-
-    Utilities.save_dataset(final, joint_output) 
+    print("joints: ", len(final[0]))
+    #print("joints dimensions: ", len(final[0]))
+    #print(len(final[0][4]))
+    if hcf:
+        print("saving as this: ", len(final), len(final[0]))
+        Utilities.save_dataset(final, joint_output, colnames=Utilities.hcf_colnames) 
+    else:
+        Utilities.save_dataset(final, joint_output)
     return final
 
 def combine_datasets(rel_data, vel_data, angle_data, images, joints_output):
@@ -564,7 +579,7 @@ def create_hcf_dataset(pre_abs_joints, abs_joints, rel_joints, abs_veljoints, im
         #hcf_cycle.append(stride_lengths[i][0])
         hcf_cycle.append(stride_lengths[i][1])
         hcf_cycle.append(max_gaps[i])
-        for c in knee_data_coeffs[0]:
+        for c in knee_data_coeffs[i]:
             hcf_cycle.append(c)
         #hcf_cycle.append(stride_ratios[i])
         print("here's the row: ", len(hcf_cycle))

Code/Programs/Machine_Learning/Model_Based/AutoEncoder/GAE.py
@@ -182,10 +182,7 @@ def cross_valid(MY_model, test_dataset, criterion=None,optimizer=None,datasets=N
         valr = i * seg + seg
         trrl = valr
         trrr = total_size
-        # msg
-#         print("train indices: [%d,%d),[%d,%d), test indices: [%d,%d)" 
-#               % (trll,trlr,trrl,trrr,vall,valr))
-        
+
         train_left_indices = list(range(trll,trlr))
         train_right_indices = list(range(trrl,trrr))
         
@@ -200,18 +197,17 @@ def cross_valid(MY_model, test_dataset, criterion=None,optimizer=None,datasets=N
         torch.manual_seed(1)
         seed = int(torch.empty((), dtype=torch.int64).random_().item())  # use the same seed as Scenario 1
         G = torch.Generator()
-        #G.manual_seed(1)
 
         #There will always be at least one dataset, use samplers made for that first dataset for all of them
         train_sample = RandomSampler(datasets[0][train_indices], generator=G)
         val_sample = RandomSampler(datasets[0][val_indices], generator=G)
-        test_sample = RandomSampler(test_dataset, generator=G)
+        test_sample = RandomSampler(test_dataset[0], generator=G)
 
         init = G.get_state()
         for dataset in datasets:
             train_set = dataset[train_indices]
             val_set = dataset[val_indices]
-            test_set = test_dataset
+            test_set = test_dataset[0]
 
             train_loaders.append(GeoLoader(train_set, batch_size=batch, sampler = train_sample, drop_last = True))
             val_loaders.append(GeoLoader(val_set, batch_size=batch, sampler = val_sample, drop_last = True))

Code/Programs/Machine_Learning/Model_Based/GCN/Dataset_Obj.py
@@ -155,7 +155,41 @@ class JointDataset(Dataset):
                 bottom_cycles.append(bottom_cycle)
 
             return top_cycles, bottom_cycles
-                
+
+        #Order should be returned: l_leg, r_leg, l_arm, r_arm, head
+        elif split_type == 5:
+            limb_cycles = [[],[],[],[],[]]
+            for cycle in (self.data_cycles):
+                single_cycle = [[],[],[],[],[]]
+                for row in cycle:
+                    row_cycle = [[],[],[],[],[]]
+                    for i, c in enumerate(single_cycle):
+                        row_cycle[i]  = [row[0], row[1], row[2]]
+                    
+                    for i, coord in enumerate(row):
+                        #Head coords 
+                        if i > 2 and i < 8:
+                            row_cycle[4].append(coord)
+                        #Left hand
+                        elif i == 8 or i == 10 or i == 12:
+                            row_cycle[2].append(coord)
+                        #Right hand
+                        elif i == 9 or i == 11 or i == 13:
+                            row_cycle[3].append(coord)
+                        #Left foot
+                        elif i == 14 or i == 16 or i == 18:
+                            row_cycle[0].append(coord)
+                        #Right foot
+                        elif i == 15 or i == 17 or i == 19:
+                            row_cycle[1].append(coord)
+                    
+                    for i, c in enumerate(row_cycle):
+                        single_cycle[i].append(c)
+
+                for i, c in enumerate(single_cycle):
+                    limb_cycles[i].append(c)
+
+            return [c for c in limb_cycles]
 
     def modify_COO_matrix(self, gait_cycle_length, connections, current_matrix):
         #Get number of joints per graph:
@@ -259,5 +293,287 @@ def data_to_graph(row, coo_matrix):
             #This isn't needed
             #edge_attr=torch.tensor(edge_attr,dtype=torch.float)
             )
+        
+        return data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class HCFDataset(Dataset):
+    def __init__(self, root, filename, test=False, transform=None, pre_transform=None, cycles = False, cycle_preset = None):
+        """
+        root = Where the dataset should be stored. This folder is split
+        into raw_dir (downloaded dataset) and processed_dir (processed data). 
+        """
+        self.test = test
+        self.filename = filename
+        self.transform = transforms.Compose([transforms.ToTensor()])
+        self.cycles = cycles
+        self.num_cycles = 0
+        self.cycle_indices = []
+        self.cycle_preset = cycle_preset
+
+        super(HCFDataset, self).__init__(root, transform, pre_transform)
+        
+    @property
+    def raw_file_names(self):
+        """ If this file exists in raw_dir, the download is not triggered.
+            (The download func. is not implemented here)  
+        """
+        return self.filename
+
+    @property
+    def processed_file_names(self):
+        """ If these files are found in raw_dir, processing is skipped"""
+        self.data = pd.read_csv(self.raw_paths[0], header=None)
+        
+        print("data being read from: ", self.raw_paths[0], type(self.raw_paths[0]))
+        if self.test:
+            return [f'data_test_{i}.pt' for i in list(self.data.index)]
+        else:
+            return [f'data_{i}.pt' for i in list(self.data.index)]
+        
+
+    def download(self):
+        pass
+
+    def process(self):
+        self.data = pd.read_csv(self.raw_paths[0], header=None)
+        self.data = convert_to_literals(self.data)
+
+        if self.cycles:
+            if self.cycle_preset != None:
+                self.data_cycles = self.cycle_preset
+            else:
+                self.data_cycles = HCF.get_gait_cycles(self.data.to_numpy(), None)
+            
+            self.data = pd.DataFrame(self.data_cycles)
+            self.num_cycles = len(self.data_cycles)
+            for index, row in enumerate(tqdm(self.data_cycles, total=len(self.data_cycles[0]))):
+                
+                self.cycle_indices.append(len(row))
+
+                # Featurize molecule
+                data = data_to_tensor(row)
+                if self.test:
+                    torch.save(data, 
+                        os.path.join(self.processed_dir, 
+                                    f'data_test_{index}.pt'))
+                else:
+                    torch.save(data, 
+                        os.path.join(self.processed_dir, 
+                                    f'data_{index}.pt'))
+                
+                t_data = torch.load(os.path.join(self.processed_dir, 
+                        f'data_{index}.pt'))             
+        else:
+
+            for index, row in tqdm(self.data_cycles.iterrows(), total=self.data_cycles.shape[0]):
+                # Featurize molecule
+                data = data_to_tensor(row)
+
+                if self.test:
+                    torch.save(data, 
+                        os.path.join(self.processed_dir, 
+                                    f'data_test_{index}.pt'))
+                else:
+                    torch.save(data, 
+                        os.path.join(self.processed_dir, 
+                                    f'data_{index}.pt'))
+
+    def len(self):
+        return self.data.shape[0]
+
+    def get(self, idx):
+
+        if self.cycles:
+            #print("calling inside self cycles", idx)
+            frame_count = 0
+            for i, c in enumerate(self.cycle_indices):
+                if idx <= frame_count:
+                    true_indice = i
+                    break
+                else:
+                    frame_count += c
+
+            #print("true indice: ", idx, true_indice)
+            #idx = true_indice
+
+    
+        if self.test:
+            data = torch.load(os.path.join(self.processed_dir, 
+                                 f'data_test_{idx}.pt'))
+        else:
+            data = torch.load(os.path.join(self.processed_dir, 
+                                 f'data_{idx}.pt'))    
+
+        #print("returned data", data, data.y, idx, true_indice)    
+        return data
+    
+    def split_cycles(self, split_type = 2):
+        if split_type == 2:
+            top_cycles = []
+            bottom_cycles = []
+            for cycle in (self.data_cycles):
+                top_cycle = []
+                bottom_cycle = []
+                for row in cycle:
+                    top_cycle_row = [row[0], row[1], row[2]]
+                    bottom_cycle_row = [row[0], row[1], row[2]]
+                    for i, coord in enumerate(row):
+                        if i > 2 and i < 14:
+                            top_cycle_row.append(coord)
+                        elif i >= 14:
+                            bottom_cycle_row.append(coord)
+                    
+                    top_cycle.append(top_cycle_row)
+                    bottom_cycle.append(bottom_cycle_row)
+                
+                top_cycles.append(top_cycle)
+                bottom_cycles.append(bottom_cycle)
+
+            return top_cycles, bottom_cycles
+
+        #Order should be returned: l_leg, r_leg, l_arm, r_arm, head
+        elif split_type == 5:
+            limb_cycles = [[],[],[],[],[]]
+            for cycle in (self.data_cycles):
+                single_cycle = [[],[],[],[],[]]
+                for row in cycle:
+                    row_cycle = [[],[],[],[],[]]
+                    for i, c in enumerate(single_cycle):
+                        row_cycle[i]  = [row[0], row[1], row[2]]
+                    
+                    for i, coord in enumerate(row):
+                        #Head coords 
+                        if i > 2 and i < 8:
+                            row_cycle[4].append(coord)
+                        #Left hand
+                        elif i == 8 or i == 10 or i == 12:
+                            row_cycle[2].append(coord)
+                        #Right hand
+                        elif i == 9 or i == 11 or i == 13:
+                            row_cycle[3].append(coord)
+                        #Left foot
+                        elif i == 14 or i == 16 or i == 18:
+                            row_cycle[0].append(coord)
+                        #Right foot
+                        elif i == 15 or i == 17 or i == 19:
+                            row_cycle[1].append(coord)
+                    
+                    for i, c in enumerate(row_cycle):
+                        single_cycle[i].append(c)
+
+                for i, c in enumerate(single_cycle):
+                    limb_cycles[i].append(c)
+
+            return [c for c in limb_cycles]
+
+    def modify_COO_matrix(self, gait_cycle_length, connections, current_matrix):
+        #Get number of joints per graph:
+        #17 connections is mid-hip appended full graph
+        current_matrix = copy.deepcopy(current_matrix)
+        max_value = 0
+        #Quickly get number of nodes in graph as this can vary depending on joints
+        for c in connections:
+            if c[0] > max_value:
+                max_value = c[0]
+            if c[1] > max_value:
+                max_value = c[1]
+
+        graph_size = max_value
+
+        for i in range(gait_cycle_length):
+            #Don't append connections to the last frame as there's nothing after it
+            if i != gait_cycle_length - 1: # -1 to ignore the last one
+                for node_index in range(graph_size):
+                    #Add connections pointing to the next graph
+                    for j in range(0, 3):
+                        current_matrix[0] += [node_index + (((i + 1) * graph_size) + 1), node_index]
+                        current_matrix[1] += [node_index , node_index + (((i + 1) * graph_size) + 1)]
+
+
+            #After connections between graphs are added, add a new matrix onto the bottom of another graph
+            if i != 0: # Ignore the first one: the original coo matrix already has it's first self-connections
+                for connection in connections:
+                    for i in range(0, 3):
+                        current_matrix[0] += [connection[0] + (((i + 1) * graph_size) + 1), connection[1] + (((i + 1) * graph_size) + 1)]
+                        current_matrix[1] += [connection[1] + (((i + 1) * graph_size) + 1), connection[0] + (((i + 1) * graph_size) + 1)]
+
+                
+        return current_matrix
+    
+#Input here would be each row
+def data_to_tensor(row):
+    #The single instance per row case (No gait cycles, row is a single frame)
+    if isinstance(row[0], np.ndarray) == False and isinstance(row[0], list) == False:
+        refined_row = row.iloc[3:]
+        node_f= refined_row
+
+        #This is standard Data that has edge shit
+        row_as_array = np.array(node_f.values.tolist())
+
+        #Turn into one-hot vector
+        y = int(row.iloc[2])
+
+        data = Data(x=torch.tensor(row_as_array, dtype=torch.float),
+                    y=torch.tensor([y], dtype=torch.long))
+        
+        return data
+    #The multi-instance case (with gait cycles, row is a full cycle of frames or varying length)
+    else:
+        gait_cycle = []
+        y_arr = []
+        for cycle_part in row:
+            refined_row = cycle_part[3:]    
+            row_as_array = np.array(refined_row)     
+            y = int(cycle_part[2])  
+
+            y_arr.append(y)
+            if len(gait_cycle) <= 0:
+                gait_cycle = row_as_array
+            else:   
+                gait_cycle = np.concatenate((gait_cycle, row_as_array), axis=0)
+
+        
+        #Verify gait cycle is calculated correctly:
+        #Pass entire cycle as single graph
+        data = Data(x=torch.tensor(list(gait_cycle), dtype=torch.float),
+            y=torch.tensor([y], dtype=torch.long))
         
         return data

Code/Programs/Machine_Learning/Model_Based/GCN/Graph_Nets.py
@@ -273,7 +273,7 @@ def train(model, loader, val_loader, test_loader, generator):
     optimizer = torch.optim.Adam(model.parameters(),
                                 lr=0.0001,
                                 weight_decay=0.00005)
-    epochs = 50
+    epochs = 250
     model.train()
 
     # Data for animations
@@ -392,8 +392,8 @@ def test(model, loaders, generator, train = False):
 
         #_, out = model(data.x, data.edge_index, data.batch, train)
         _, out = model(data_x, data_i, data_b, train)
-        loss += criterion(out, data.y) / len(loaders)
-        #print("acc: ", acc, out, data.y)
-        acc += accuracy(out.argmax(dim=1), data.y) / len(loaders)
+        loss += criterion(out, data.y) / len(loaders[0])
+        #print("acc: ", acc, data.y)
+        acc += accuracy(out.argmax(dim=1), data.y) / len(loaders[0])
 
     return loss, acc